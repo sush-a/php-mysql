b67c0d8ad7d1e72915dada3729d152d060198260
Nataraj G <mail2natti@gmail.com>
2022-09-02 18:20:30 +0530

    initial commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'database-1.ceaqyubwm5tx.ap-south-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'employees_db');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
